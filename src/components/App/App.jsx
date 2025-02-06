d95e176ab2a32e05ff360576a93d682ae1514303
Олег <olegsereda898@gmail.com>
2025-02-06 10:11:16 +0200

    add suspense

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,35 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "../../pages/Home";
-import About from "../../pages/About";
-import Products from "../../pages/Products";
 import NotFound from "../../pages/NotFound";
 import css from "./App.module.css";
-import ProductDetails from "../../pages/ProductDetails";
-import { Mission } from "../Mission";
-import { Team } from "../Team";
-import { Reviews } from "../Reviews";
 import { AppBar } from "../AppBar/AppBar";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("../../pages/Home"));
+const About = lazy(() => import("../../pages/About"));
+const Products = lazy(() => import("../../pages/Products"));
+const ProductDetails = lazy(() => import("../../pages/ProductDetails"));
+const Mission = lazy(() => import("../Mission"));
+const Team = lazy(() => import("../Team"));
+const Reviews = lazy(() => import("../Reviews"));
 
 export const App = () => {
   return (
     <div className={css.container}>
       <AppBar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />}>
-          <Route path="mission" element={<Mission />} />
-          <Route path="team" element={<Team />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />}>
+            <Route path="mission" element={<Mission />} />
+            <Route path="team" element={<Team />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
